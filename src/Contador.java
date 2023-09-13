09502ea320fbece2f1278021d7b1b86f5f68e47b
Vinicius Santos <vinisantoosk@gmail.com>
2023-09-13 00:09:12 -0300

    Fisrt commit

---
+++ src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        var terminal = new Scanner(System.in);
+
+        System.out.println("Digite o valor do primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o valor do segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + (i + 1));
+        }
+        // realizar o for para imprimir os números com base na variável contagem
+    }
+}
